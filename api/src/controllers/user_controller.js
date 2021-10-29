3bf5ed40b868127e6a785e61779892864def7b6b
duchao3003 <30766129+duchao11280@users.noreply.github.com>
2021-10-29 22:34:03 +0700

    API  đăng kí

--- api/src/controllers/user_controller.js
+++ api/src/controllers/user_controller.js
@@ -1,11 +1,12 @@
 const UserModel = require('../models/user_model');
-
+var bcrypt = require("bcryptjs");
 exports.getAllUsers = (req,res)=>{
     UserModel.getAllUsers((err,users)=>{
         if(err) throw err;
         res.send(users);
     });
 }
+
 // get user info by ID
 exports.getUserByID = (req,res)=>{
     UserModel.getUserByID(req.params.id, (err,user)=>{
@@ -26,6 +27,24 @@ exports.updateUser = (req,res)=>{
                 res.send(err);
             }
             res.json({status:true, message:'User updated successfully',data: user.insertID})
+        })
+    }
+}
+
+// Sign up
+exports.signUp = (req,res)=>{
+    const userReqData = new UserModel(req.body);
+    userReqData.password = bcrypt.hashSync(req.body.password, 14);
+    console.log('user req data update',userReqData);
+    if(req.body.contructor == Object && Object.keys(req.body).length==0){
+        res.send(400).send({success:false, message:'Empty'});
+    }else{
+        UserModel.insertUser(userReqData.userName,userReqData.password,userReqData.fullName,
+            userReqData.email,userReqData.phonenumber,userReqData.role,(err,user)=>{
+            if(err){
+                res.send(err);
+            }
+            res.json({status:true, message:'Đăng kí thành công',data: user.insertID})
         })
     }
 }
